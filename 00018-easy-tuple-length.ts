48b8030e034c348bca53e7c98ec0a283ce9b833f
Deyaa Pozan <dpozan@connectedmotion.io>
2025-03-11 01:46:14 +0300

    solve 00018-easy-tuple-length.ts

---
+++ 00018-easy-tuple-length.ts
@@ -0,0 +1,24 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
+
+
+// ============= Your Code Here =============
+type Length<T extends readonly unknown[]> = T['length']
+
+
+// ============= Description =============
+// T['length']: Accesses the length property of the tuple/array type
+// For example, the type [string, number, boolean]['length'] evaluates to the type 3
+
