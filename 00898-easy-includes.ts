51a5b645684804ad3a94afe3de2c6553462aefe6
Deyaa Pozan <dpozan@connectedmotion.io>
2025-03-16 02:43:14 +0300

    solve 00898-easy-includes

---
+++ 00898-easy-includes.ts
@@ -0,0 +1,40 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">, true>
+  >,
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: "A" }], { readonly a: "A" }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: "A" }], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>
+];
+
+// ============= Your Code Here =============
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
+
+// ============= Description =============
+// T extends [infer First, ...infer Rest] syntax lets you extract the first element of a tuple type.
+// Equal<First, U> extends true ? true : Includes<Rest, U> syntax lets you check if the first element of a tuple type is equal to a given value.
+// If the first element is equal to the given value, the type true is returned, otherwise, the type Includes<Rest, U> is returned.
